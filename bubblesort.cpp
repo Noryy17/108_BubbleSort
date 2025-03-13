8479a644778424b3ea21868d8122f2903d3e0e07
Faid arya <faid.arya.ft24@mail.umy.ac.id>
2025-03-13 17:47:09 +0700

    belajar display data

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -2,47 +2,46 @@
 using namespace std;
 
 int arr[15];
-int n;
+int n; //Masok jumlah data
 
-void inputData()
+void InputData()
 {
-
     while (true)
     {
-        cout << "Masukan jumlah data:";
+        cout << "masukan jumlah data: ";
         cin >> n;
-        if (n < 15)
-        {
+        
+        if (n < 15){
             break;
         }
-
         else
         {
-            cout << "Jumlah data maksimal 15" << endl;
+            cout << "jumlah data maksimal 15" << endl;
         }
+    }
 
-        cout << "=====================" << endl;
-        cout << "Masukan data" << endl;
-        cout << "=====================" << endl;
-        cout << endl;
+    cout << "===================" << endl;
+    cout << "masukan data" << endl;
+    cout << "===================" << endl;
+    cout << endl;
 
-        for (int i = 0; i < n; i++)
-        {
-            cout << "Masukan data ke-" << i + 1 << " = ";
-            cin >> arr[i];
-        }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "masukan data ke-" << i + 1 << "=";
+        cin >> arr[i];
     }
 }
 
-void bubblesort()
+void BubbleSort()
 {
-    int pass = 1;
+    int pass = 1; //Langkah 1 cuy
     do
     {
-        for (int j = 0; j <= n - 1 - pass; j++)
-
+    
+        for (int  j = 0; j <= n - 1 - pass; j++) //Langkah 2 cuy
         {
-            if (arr[j] > arr[j + 1])
+            if(arr[j] > arr[j + 1]) //Langkah 3 cuy
             {
                 int temp;
                 temp = arr[j];
@@ -50,12 +49,30 @@ void bubblesort()
                 arr[j + 1] = temp;
             }
         }
-
+    
         pass = pass + 1;
-    } while (pass <= n - 1);
+        
+    } while (pass <= n - 1); //Langkah 6 cuy
+    
 }
-int main()
-{
 
-    inputData();
+void DisplayData(){
+    cout << endl;
+    cout << "===============================" << endl;
+    cout << "Data setelah diurutkan" << endl;
+    cout << "===============================" << endl;
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << i + 1 << "=" << arr[i] << endl;
+    }
+    
+}
+
+int main ()
+{
+    InputData();
+    BubbleSort();
+    DisplayData();
+    return 0;
 }
